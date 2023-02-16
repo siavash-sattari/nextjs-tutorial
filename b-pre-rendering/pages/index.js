6caebc0de8d31d02cab637ce04effc84c303975a
siavash sattari <siavash.sattari.dev@gmail.com>
2023-02-16 05:54:25 -0800

    1 - using getStaticProps

--- b-pre-rendering/pages/index.js
+++ b-pre-rendering/pages/index.js
@@ -1,5 +1,27 @@
-const Homepage = () => {
-  return <h1>Pre Rendering</h1>;
+const Homepage = props => {
+  const { products } = props;
+
+  return (
+    <>
+      <h1>Home Page</h1>
+      <ul>
+        {products.map(item => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
+
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [
+        { id: 'p1', title: 'product1' },
+        { id: 'p2', title: 'product2' }
+      ]
+    }
+  };
+}
 
 export default Homepage;
