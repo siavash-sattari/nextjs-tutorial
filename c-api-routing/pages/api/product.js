7eba80d0a1a5d040e039eaffc4ce0e55bc4ab40e
siavash sattari <siavash.sattari.dev@gmail.com>
2023-02-17 07:38:43 -0800

    1 - api route / sending request to api

---
+++ c-api-routing/pages/api/product.js
@@ -0,0 +1,12 @@
+function handler(req, res) {
+  if (req.method === 'POST') {
+    const title = req.body.title;
+    const price = req.body.price;
+    const newProduct = { title, price };
+    const products = [];
+    products.push(newProduct);
+    console.log(products);
+  }
+}
+
+export default handler;
--- c-api-routing/pages/index.js
+++ c-api-routing/pages/index.js
@@ -1,5 +1,37 @@
+import { useRef } from 'react';
+
 const Homepage = () => {
-  return <h1>Home page</h1>;
+  const titleInputRef = useRef();
+  const priceInputRef = useRef();
+
+  const addProductHandler = async event => {
+    event.preventDefault();
+    const title = titleInputRef.current.value;
+    const price = priceInputRef.current.value;
+    const reqBody = { title, price };
+    const response = await fetch('/api/product', {
+      method: 'POST',
+      body: JSON.stringify(reqBody),
+      headers: {
+        'content-type': 'application/json'
+      }
+    });
+  };
+
+  return (
+    <div>
+      <h1>Create New Product</h1>
+      <form onSubmit={addProductHandler}>
+        <div>
+          <input type='text' placeholder='Title' ref={titleInputRef} />
+        </div>
+        <div>
+          <input type='text' placeholder='Price' ref={priceInputRef} />
+        </div>
+        <button>Add</button>
+      </form>
+    </div>
+  );
 };
 
 export default Homepage;
